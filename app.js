81a9e757fab1b29bde39b8a07ed25fe2a6f0a62b
unknown <victoryakubu09@gmail.com>
2022-07-07 21:58:58 +0100

    level 2 encryption, add env file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
@@ -16,8 +17,7 @@ const userSchema = new mongoose.Schema({
     password:String
     // whatever else
 });
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret,    encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {secret: process.env.SECRET,    encryptedFields: ["password"] });
 
 
 const User = mongoose.model("User", userSchema);
